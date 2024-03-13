fcf731d85833b05aeb613517798b12afdd36199d
Rpgslayer <alexvoidoc@gmail.com>
2024-03-13 03:32:37 -0400

    Started education section

--- app.py
+++ app.py
@@ -6,11 +6,12 @@ from flask import render_template
 app = Flask(__name__, static_url_path='/static')
 
 
-# @app.route("/about/")
-# def about():
-#     return render_template("about.html")
-
-
 @app.route("/")
 def home():
     return render_template("home.html")
+
+
+@app.route("/education")
+def education():
+    return render_template("education.html")
+
